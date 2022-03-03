5c6f918ea2b65baccc80378597ee8136edc5e17f
Abdulaziz930 <abdulazizak@code.edu.az>
2022-03-03 17:12:27 +0400

    first commit

---
+++ P203/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace P203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ctrl + shift + b => build
+            //ctrl + k + c comment
+            //ctrl + k + u uncomment
+            //ctrl + k + d prettify
+
+            #region Variables
+
+            //int number = 12;
+            //number = 10;
+            //Console.WriteLine(number++);
+
+            //string word = "lorem";
+            //word = "salam";
+
+            //Console.WriteLine(word);
+
+            //int number1 = 10;
+            //int number2 = 5;
+            //number1 = number1 + number2; //10 + 5 = 15
+            //number2 = number1 - number2; //15 - 5 = 10 
+            //number1 = number1 - number2; // 15 - 10 = 5
+
+            //Console.WriteLine("number1: " + number1);
+            //Console.WriteLine("number2: " + number2);
+
+            //double a = 4.5;
+            //a = 5;
+
+            //bool isDeleted = true;
+            //isDeleted = false;
+
+            //Console.WriteLine(isDeleted);
+
+            //float b = 4.5f;
+            //Console.WriteLine(b);
+
+            //decimal c = 4.5m;
+            //Console.WriteLine(c);
+
+            //char c = 'a';
+            //Console.WriteLine(c);
+
+            //object o = "salam";
+            //o = 5;
+
+            //var i = true;
+            //i = false;
+
+            //Console.WriteLine(i);
+
+            //dynamic y = "salam";
+            //y = 5;
+            //y = true;
+            //y = 4.5;
+
+            //Console.WriteLine(y);
+
+            #endregion
+
+            #region Conditions
+
+            //int age = 17;
+
+            //if (age > 16)
+            //{
+            //    Console.WriteLine("yeniyetmedir");
+            //}else if (age == 16)
+            //{
+            //    Console.WriteLine("kecid merhelesindedir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("yashamir");
+            //}
+
+            //if(age > 16)
+            //{
+            //    Console.WriteLine("test");
+            //}else if(age == 16)
+            //{
+            //    Console.WriteLine("Test1");
+            //}
+
+            //if (age > 16)
+            //{
+            //    Console.WriteLine("test");
+            //}
+
+            //if (age == 16)
+            //{
+            //    Console.WriteLine("Test1");
+            //}
+
+            //switch (age)
+            //{
+            //    case 16:
+            //        Console.WriteLine("16");
+            //        break;
+            //    case 17:
+            //        Console.WriteLine("17");
+            //        break;
+            //    case 18:
+            //        Console.WriteLine("18");
+            //        break;
+            //    default:
+            //        Console.WriteLine("default");
+            //        break;
+            //}
+
+            #endregion
+
+            #region Loop
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int[] numberArr = { 1, 20, 3, 4, 5, 6 };
+            //for (int i = 0; i < numberArr.Length; i++)
+            //{
+            //    Console.WriteLine(numberArr[i]);
+            //}
+            //int sum = 0;
+            //foreach (int number in numberArr)
+            //{
+            //    if (number % 2 == 0) 
+
+            //    {
+            //        sum = sum + number;
+            //    }
+            //    //Console.WriteLine(number);
+            //}
+
+            //Console.WriteLine(sum);
+
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //while (true)
+            //{
+            //    Console.WriteLine("test");
+            //}
+
+            //int i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //} while (i < 0);
+            #endregion
+        }
+    }
+}
